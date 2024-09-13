46d38409ed8f2e233fdfacfd1194fb2bd01edb91
Scott Sandilands <sosandilands@gmail.com>
2024-09-13 17:58:07 -0500

    Commit javascript

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,34 @@
 <script setup>
   import {ref} from 'vue'
-    
+
+  const timeInput = ref(null)
+  const countdownText = ref('')
+  let timer = null
+
+  const startTimer = () =>{
+    let time = parseInt(timeInput.value)
+
+    if (isNaN(time) || time < 1 || time > 60) {
+      alerts("Please Enter a Value Between 1 and 60")
+      return
+    }
+
+    //clear any existing timer
+    if (timer) {
+      clearInterval(timer)
+    }
+
+    //start the countdown
+    timer = setInterval(() => {
+      time--
+      countdownText.value = time
+
+      if (time <= 0) {
+        clearInterval(timer)
+        countdownText.value = "Timer has ended"
+      }
+    }, 1000)
+  }
 </script>
 
 <template>
